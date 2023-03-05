b30b46a989406e74008a9a2a60c431f76a2425db
kakigrat12 <86928317+kakigrat12@users.noreply.github.com>
2023-03-05 19:43:26 +0300

    Movement
    Update

--- Kirill_Tanks/Assets/Scripts/Movement.cs
+++ Kirill_Tanks/Assets/Scripts/Movement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody _rigidbody;
+
     void Start()
     {
         
